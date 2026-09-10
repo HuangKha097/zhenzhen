d6dd1440c0105948c6d883c640709076f2087e97
hoang kha <huangkha097@gmail.com>
2026-09-10 14:22:30 +0700

    Prject Name Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Ghost AI 
-    <Button  >Click me</Button>
-  </div>;
-
+  return (
+    <div>
+      Zhenzhen
+      <Button>Click me</Button>
+    </div>
+  );
 }
-
